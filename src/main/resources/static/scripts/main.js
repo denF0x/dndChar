daa66d40658b39055ad69d4dc2f82d59b7c4b30f
Denis <babkin.denys@gmail.com>
2020-07-23 18:26:43 +0300

    after color changes

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,3 +1,35 @@
+
+
+/*function changeTheme() {
+
+let page = document.querySelector('.page');
+// let button = document.querySelector('.theme-button');
+
+
+    page.classList.toggle('light-theme');
+    page.classList.toggle('dark-theme');
+    console.log("I'm alive");
+
+
+}
+
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}*/
+
+/*
+
+    let page = document.querySelector('body');
+    let button = document.querySelector('.theme-button');
+
+    page.classList.toggle('light-theme');
+    page.classList.toggle('dark-theme');*/
+
+
 // let message = document.querySelector('.subscription-message');
 /*
 let form = document.querySelector('.subscription');
@@ -9,26 +41,26 @@ form.onsubmit = function(evt) {
 */
 
 
-// let page = document.querySelector('.page');
-// let button = document.querySelector('.theme-button');
-//
-// button.onclick = function () {
-//     page.classList.toggle('light-theme');
-//     page.classList.toggle('dark-theme');
-//     console.log("I'm alive");
-//
-// }
+
 
 
 //
 // background:#ffffff url("img/bg-page.png") no-repeat top center
 
-function changeTheme() {
-    let page = document.querySelector('.page');
-    let button = document.querySelector('.theme-button');
 
-    page.classList.toggle('light-theme');
-    page.classList.toggle('dark-theme');
-    console.log("I'm alive");
+/*let lightColor = "#f2edd0";
+    let darkColor = "#59514f";
+    let text = document.querySelector('text');
 
-}
+    let currentColor =  rgb2hex
+    (window.getComputedStyle(document.body, null)
+        .getPropertyValue('background-color'));
+    console.log(currentColor);
+
+    if(currentColor === lightColor){
+        document.body.style.background = darkColor;
+
+    }
+    if(currentColor === darkColor){
+        document.body.style.background = lightColor;
+    }*/
